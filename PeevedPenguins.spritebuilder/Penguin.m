b1a9804edb57c68d6f7cbab6289e94ffce2fea38
Daniel Gallardo <dgallardo626@gmail.com>
2014-06-27 11:33:37 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,23 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Danny on 6/27/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init{
+    self = [super init];
+    
+    if(self){
+        CCLOG(@"Penguin Created");
+    }
+    return self;
+}
+
+
+@end
