47bf35e390ba64040b4f18dcefe17520aa7b99be
Daniel Gallardo <dgallardo626@gmail.com>
2014-06-27 16:12:33 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -11,11 +11,15 @@
 @implementation Gameplay{
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
+    CCNode *_levelNode;
 }
 
 - (void)didLoadFromCCB {
     //tell this scene to accept touches
     self.userInteractionEnabled = YES;
+    
+    CCScene *level = [CCBReader loadAsScene:@"levels/Level1"];
+    [_levelNode addChild:level];
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
@@ -27,7 +31,7 @@
     CCNode *penguin = [CCBReader load:@"Penguin"];
     
     //position the penguin at the bowl of the catapult
-    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16 , 50));
     
     //add the penguin to the physicsNode of this scene (because it has physics enabled)
     [_physicsNode addChild:penguin];
@@ -37,5 +41,8 @@
     CGPoint force = ccpMult(launchDirection, 8000);
     [penguin.physicsBody applyForce:force];
 }
+
+
+
 
 @end
