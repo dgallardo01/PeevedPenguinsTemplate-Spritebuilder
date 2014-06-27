f36bbf07c5c7657d8767f8b1cbbf24c9aa1dd12e
Daniel Gallardo <dgallardo626@gmail.com>
2014-06-27 12:04:29 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -13,6 +13,8 @@
 
 - (void)play{
     CCLOG(@"play button pressed");
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
 
 @end
