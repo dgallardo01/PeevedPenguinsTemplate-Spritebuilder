ab5e25c14251a2cb53392d978e93a84fd318cde8
Daniel Gallardo <dgallardo626@gmail.com>
2014-07-04 23:20:45 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -172,7 +172,7 @@ static const float MIN_SPEED = 5.f;
 
 - (void)update:(CCTime)delta{
     //if speed is below minimum speed, assume this attempt is over
-    if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED) {
+    if (_currentPenguin.launched) {
         [self nextAttempt];
         return;
     }
