8c06804ee429d7f386489e038cc09cb03475bccd
Daniel Gallardo <dgallardo626@gmail.com>
2014-07-04 22:53:42 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -21,6 +21,7 @@ static const float MIN_SPEED = 5.f;
     CCNode *_currentPenguin;
     CCPhysicsJoint *_mouseJoint;
     CCPhysicsJoint *_penguinCatapultJoint;
+    CCAction *_followPenguin;
 }
 
 - (void)didLoadFromCCB {
@@ -96,8 +97,8 @@ static const float MIN_SPEED = 5.f;
         _currentPenguin.physicsBody.allowsRotation = YES;
         
         //follow the flying penguin
-        CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
-        [_contentNode runAction:follow];
+        _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+        [_contentNode runAction:_followPenguin];
     }
 }
 
@@ -164,6 +165,36 @@ static const float MIN_SPEED = 5.f;
     
     //finally, remove the destroyed seal
     [seal removeFromParent];
+}
+
+- (void)update:(CCTime)delta{
+    //if speed is below minimum speed, assume this attempt is over
+    if (ccpLength(_currentPenguin.physicsBody.velocity) < MIN_SPEED) {
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMin = _currentPenguin.boundingBox.origin.x;
+    
+    if (xMin < self.boundingBox.origin.x) {
+        [self nextAttempt];
+        return;
+    }
+    
+    int xMax = xMin + _currentPenguin.boundingBox.size.width;
+    
+    if(xMax > (self.boundingBox.origin.x + self.boundingBox.size.width)){
+        [self nextAttempt];
+        return;
+    }
+}
+
+- (void)nextAttempt{
+    _currentPenguin = nil;
+    [_contentNode stopAction:_followPenguin];
+    
+    CCActionMoveTo *actionMoveTo = [CCActionMoveTo actionWithDuration:1.f position:ccp(0,0)];
+    [_contentNode runAction:actionMoveTo];
 }
 
 @end
